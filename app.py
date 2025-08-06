199a0a931db383813af8b900be5f68f93c5de567
zzang <cvcv6226@gmail.com>
2025-08-06 11:10:20 +0900

    app.py update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import streamlit as st
+import pandas as pd
 import torch
 import numpy as np
 import pickle
 from transformers import CLIPProcessor, CLIPModel
 import streamlit as st
-
+from PIL import Image
 
 ###################함수 정의######################
 
@@ -28,14 +28,28 @@ def load_model_and_data():
     return model, processor, image_embedding, image_paths
 
 # 텍스트 임베딩 - 이미지 임베딩 유사도 계산 함수
-def similarty_caluation(text_embedding, image_embedding):
+def similarity_caluation(text_embedding, image_embedding, image_path):
     sim_score_lst = []
-    for num in range(len(image_embedding)):
-        torch.nn.functional.cosin
+    for idx in range(len(image_embedding)):
+        similarity = torch.nn.functional.cosine_similarity(text_embedding, image_embedding[idx])
+        sim_score_lst.append(round(similarity.item(), 3))
+
+    similarity_df = pd.DataFrame(
+        {
+            'Score' : sim_score_lst,
+            'Image_path' : image_path
+        }
+    )
+    similarity_df = similarity_df.sort_values('Score', ascending=False).head(5)
+    similarity_df['Rank'] = [num + 1 for num in range(len(similarity_df))]
+    similarity_df.set_index('Rank', inplace=True)
+
+    return similarity_df
 
 #################UI 구성###############################
 
-st.title('이미지 검색 엔진')
+# st.title('이미지 검색 엔진')
+st.markdown("<h1 style='text-align: center; color: black;'>이미지 검색 엔진</h1>", unsafe_allow_html=True)
 
 st.write('모델 및 데이터를 메모리에 로드중...')
 
@@ -57,14 +71,27 @@ if text_query :
 
     # 단일 검색어를 모델에 넣어 처리
     with torch.no_grad():
-        inputs = processor(text = text_query, return_tensor = 'pt', padding = True)
+        inputs = processor(text = text_query, return_tensors = 'pt', padding = True)
 
         # get_text_features 메서드로 텍스트 임베딩만 추출
         text_features = model.get_text_features(**inputs)
 
     st.success('텍스트 임베딩 생성완료')
 
-# 불러오기 한 이미지 임베딩 numpy 배열 -> tensor 변환
-embedding_tensor = torch.from_numpy(image_embedding)
+    # 불러오기 한 이미지 임베딩 numpy 배열 -> tensor 변환
+    embedding_tensor = torch.from_numpy(image_embedding)
 
+    # 텍스트-이미지 유사도 계산
+    sim_top5 = similarity_caluation(text_features, embedding_tensor, image_path)
 
+    # 유사도 결과 전시
+    # st.header('상위 5개 이미지 유사도 결과')
+    st.markdown("<h2 style='text-align: center; color: black;'>상위 5개 이미지 유사도 결과</h1>", unsafe_allow_html=True)
+    st.dataframe(sim_top5['Score'])
+
+    # 상위 5개 이미지 불러오기
+    top5_path = [path for path in sim_top5['Image_path']]
+
+    for num in range(len(top5_path)) :
+        img_object = Image.open(top5_path[num])
+        st.image(img_object, caption = f'후보 {num + 1}순위')
